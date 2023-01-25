de90e5790950925fdc3d9586e3eda4d0bdfbc6c9
oldman07 <backagain010@gmail.com>
2023-01-25 18:09:49 +0500

    tutus short code with static text

---
+++ tutsplus-hooked-plugin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* Plugin Name:   Call to Action Plugin - Hooked to Theme
+* Plugin URI:    https://tutsplus.com/
+* Description:   Adds a call to action box
+* Version:       1.0
+* Author:        Rachel McCollin
+* Author URI:    http://rachelmccollin.com
+*
+*
+*/
+
+/*********************************************************************************
+Enqueue stylesheet
+*********************************************************************************/
+function tutsplus_hook_plugin_enqueue_styles() {
+	
+	wp_register_style( 'hook_cta_css', plugins_url( 'css/style.css', __FILE__ ) );
+	wp_enqueue_style( 'hook_cta_css' );
+	
+}
+add_action( 'wp_enqueue_scripts', 'tutsplus_hook_plugin_enqueue_styles' );
+ 
+/*********************************************************************************
+CTA box
+*********************************************************************************/
+function tutsplus_cta_below_posts() {
+	
+	if( is_singular( 'post' )) { ?>
+		
+		<div class="cta-in-post">
+			<a href="#">For more posts like this in your inbox every week, join our mailing list.</a>
+		</div>
+		
+	<?php }
+	
+}
+add_action( 'tutsplus_after_content', 'tutsplus_cta_below_posts' );
+
+/*********************************************************************************
+Simple shortcode
+*********************************************************************************/
+function tutsplus_cta_simple() { 
+	
+	ob_start();
+	?>
+	
+	<div class="shortcode cta">
+		<a href="#">For more posts like this in your inbox every week, join our mailing list.</a>
+	</div>
+	
+	<?php 
+	return ob_get_clean();
+		
+}
+add_shortcode( 'cta', 'tutsplus_cta_simple' );
